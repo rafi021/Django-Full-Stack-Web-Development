f0fa061a34ec9573d60f73b29f6a489803c19b57
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2017-10-12 20:48:12 +0600

    my code

---
+++ JS_LEVEL_ONE/myscript.js
@@ -0,0 +1,4 @@
+alert("Welcome to your Bank!")
+var desposit = prompt("How much would you like to desposit?")
+alert("You've desposited: "+desposit)
+console.log("You are a cool person.")
---
+++ JS_LEVEL_ONE/part3.js
@@ -0,0 +1,5 @@
+alert("Welcome to your First Exercise!")
+var weight_pound=prompt("Enter a weight number in Ibs: ")
+var kg=weight_pound*0.454
+alert("The given weight in Kiligrams is: "+kg)
+console.log("Conversion Completed")
