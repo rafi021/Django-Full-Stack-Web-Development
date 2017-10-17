3b023beb012fccc854bfba4fa2da7703c9334c2c
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2017-10-17 22:05:32 +0600

    My Code

--- JS_LEVEL_ONE/part3.js
+++ JS_LEVEL_ONE/part3.js
@@ -1,5 +1,4 @@
-alert("Welcome to your First Exercise!")
-var weight_pound=prompt("Enter a weight number in Ibs: ")
-var kg=weight_pound*0.454
-alert("The given weight in Kiligrams is: "+kg)
+var lbs = prompt("What is the weight in pounds (lbs)?")
+var kg = lbs*0.454
+alert("That is:  "+kg+" kilograms")
 console.log("Conversion Completed")
