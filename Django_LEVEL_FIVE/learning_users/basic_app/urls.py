260a34999aa541e24d7b2f5c0112fa38349e8ce9
Mahmud Ibrahim <mahmud.ibrahim021@gmail.com>
2018-02-21 18:57:11 +0600

    django code

---
+++ Django_LEVEL_FIVE/learning_users/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from basic_app import views
+
+# Template Urls
+app_name = 'basic_app'
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+
+]
---
+++ Django_LEVEL_FOUR/learning_templates/py/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ASUS\Desktop\django full stack\Django_LEVEL_FOUR\learning_templates\py\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
